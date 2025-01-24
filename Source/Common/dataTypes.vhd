6851eee0d05f8c48b771ac066828f0e5c35b877b
Alexandru Anastasiu <jtmstn1@gmail.com>
2025-01-24 23:11:52 +0200

    I'm still thinking...

--- Source/Common/dataTypes.vhd
+++ Source/Common/dataTypes.vhd
@@ -2,5 +2,5 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 package bfCPUDataTypes is
-    type addressBus is array(0 to 7) of std_logic;
+    type addressBus is array(0 to 15) of std_logic;
 end bfCPUDataTypes;
